5f40ad292bdb28554d15c9cb682ac22b013f9ac6
rajpratapgit <127298264+rajpratapgit@users.noreply.github.com>
2024-03-02 13:51:53 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node js not browser
+
+console.log(3 +
+    3) //code should be readable, not good practice
+
+console.log("Raj")
+
+let name = "Raj"
+let age = 19
+let isLoggedIn = true
+
+
+// number 2 to the power 63
+// bigint
+// string => use int this '' and this " " but prefer double " " 
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof age); //number
+console.log(typeof "Raj"); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefine
+
