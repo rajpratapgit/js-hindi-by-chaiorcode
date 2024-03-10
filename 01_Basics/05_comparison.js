df350ee9bf26cf46af8a0e225b6b16b376fd6f85
rajpratapgit <127298264+rajpratapgit@users.noreply.github.com>
2024-03-10 21:10:06 +0530

    Learn about comparison

---
+++ 01_Basics/05_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1); // not good practice
+
+console.log(null > 0); // false 
+console.log(null == 0); // false
+console.log(null >= 0); // true  // sometime js converts null into NaN and sometime into 0
+
+console.log(undefined > 0); // false
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+
+//  ===
+
+console.log("2" === 2); // false
+
