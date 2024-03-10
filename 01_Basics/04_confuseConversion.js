920a11b076f4fa0f310a1373d1d156fd55a755d6
rajpratapgit <127298264+rajpratapgit@users.noreply.github.com>
2024-03-10 20:52:35 +0530

    Learn more about conversion

---
+++ 01_Basics/04_confuseConversion.js
@@ -0,0 +1,36 @@
+// *** operations ***
+
+let value = 5
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Raj"
+let str2 = " Pratap"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // not 14 // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCount = 100
+gameCount++;
+console.log(gameCount);
+
