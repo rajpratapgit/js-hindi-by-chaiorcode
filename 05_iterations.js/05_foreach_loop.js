b6e0e4faa9f610fa8c4e67e71ea5a3b48971b3ab
rajpratapgit <127298264+rajpratapgit@users.noreply.github.com>
2024-05-07 20:43:06 +0530

    Learned about foreach loop

---
+++ 05_iterations.js/05_foreach_loop.js
@@ -0,0 +1,37 @@
+const coding = ["js", "swift", "java", "python", "html"]
+
+coding.forEach( function (item) {
+    console.log(item);
+} )
+
+coding.forEach( (item) => {
+    console.log(item);
+} )
+
+function printMe(item){
+    console.log(item);
+}
+coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr);
+} )
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
