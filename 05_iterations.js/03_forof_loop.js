20425302ce18bd3afd3cbbb8b87261b7326b6114
rajpratapgit <127298264+rajpratapgit@users.noreply.github.com>
2024-05-07 20:41:48 +0530

    Learned about for of loop

---
+++ 05_iterations.js/03_forof_loop.js
@@ -0,0 +1,49 @@
+// for of loop
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const myArr = [1,2,3,4,5]
+
+for (const num of myArr) {
+    console.log(num);
+}
+
+const greetings = "Hello World!"
+
+for (const greet of greetings) {
+    if(greet==" ")
+    {
+        continue;
+    }
+    else
+    {
+    console.log(`Each char is ${greet}`);
+    }
+}
+
+// Maps
+
+const map = new Map()
+map.set('In', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+map.set("In", "India"); // this will not go into map bcz map are known for unique  value
+console.log(map);
+
+for (const [key, value ] of map) {
+    console.log(key, ":-", value);
+}
+
+const myObject = {
+    gameOne: "NFS",
+    gametwo: "SpiderMan"
+}
+
+
+// Error
+// for (const [key, value] of myObject) { 
+//     console.log(key, ":-", value);
+// }
+
+
