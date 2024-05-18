73f68ea2e877e99ed3e40c320a68032a42a6867e
Raj Pratap <127298264+rajpratapgit@users.noreply.github.com>
2024-05-18 17:58:20 +0530

    Learned more about objects

--- 10_classAndOOPS/09_mathpi.js
+++ 10_classAndOOPS/09_mathpi.js
@@ -1,3 +1,33 @@
-// Object.getOwnPropertyDescriptor
+const descriptor = Object.getOwnPropertyDescriptor(Math, 'PI')
+
+console.log(descriptor);
 
 console.log(Math.PI);
+Math.PI = 5
+console.log(Math.PI);
+
+const coffee = {
+    name: "Cold Coffee",
+    price: "399",
+    isAvailable: true,
+
+    orderCoffee: function(){
+        console.log("Ordering coffee");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(coffee, "name"));
+
+Object.defineProperty(coffee, "name", {
+    writable: false,
+    enumerable: false,
+    configurable: false
+})
+
+// console.log(Object.getOwnPropertyDescriptor(coffee, "name"));
+
+for (let [key, value] of Object.entries(coffee)) {
+    if(typeof value != 'function'){
+    console.log(`${key}: ${value}`);
+    }
+}
