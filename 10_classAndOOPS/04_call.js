d7308e82b56616cfde44a9b74f447d34f6a65027
Raj Pratap <127298264+rajpratapgit@users.noreply.github.com>
2024-05-18 16:47:13 +0530

    Learned about call()

---
+++ 10_classAndOOPS/04_call.js
@@ -0,0 +1,15 @@
+function setUserName(username){
+    this.username = username
+    console.log("Called");
+}
+
+function createUser(username, email, password){
+    setUserName.call(this, username)
+    this.email = email
+    this.password = password
+    
+}
+
+const chai = new createUser("chai", "chai@example.com", "123")
+
+console.log(chai);
